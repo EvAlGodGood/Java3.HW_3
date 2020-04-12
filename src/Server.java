c3bdf576d9fd340eab274c80305bf4e9cc718364
EvAlGodGood <Gooodfile@mail.ru>
2020-04-12 10:05:04 +0800

    HW3. Сделаны 1,2 и 4 частично (проблема с чтением /передачей, содержимое файла коверкается и из переданного файла не получается восстановить объект)

--- src/Server.java
+++ src/Server.java
@@ -1,32 +1,32 @@
-//import java.io.*;
-//import java.net.ServerSocket;
-//import java.net.Socket;
-//
-//public class Server implements Runnable {
-//
-//    public Server () /*throws IOException*/{
-//    }
-//
-//    public void run(){
-//        Socket socket = null;
-//
-//        try (ServerSocket serverSocket = new ServerSocket(3746)) {
-//            System.out.println("Сервер запущен, ожидаем подключения...");
-//            socket = serverSocket.accept(); //ждем клиента
-//            System.out.println("Клиент подключился");
-//
-//            DataInputStream in = new DataInputStream(socket.getInputStream());
-//            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-//            String str = in.readUTF(); //получение сообщения от клиента
-//            FileWriter wr = new FileWriter("cat1.ser");
-//            wr.write(str);
-//            //System.out.println("Сервер получил: " + str);
-//            //out.writeUTF("Привет-привет..."); //ответ сервера
-//            wr.close();
-//
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//    }
-//}
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server implements Runnable {
+
+    public Server () /*throws IOException*/{
+    }
+
+    public void run(){
+        Socket socket = null;
+
+        try (ServerSocket serverSocket = new ServerSocket(3746)) {
+            System.out.println("Сервер запущен, ожидаем подключения...");
+            socket = serverSocket.accept(); //ждем клиента
+            System.out.println("Клиент подключился");
+
+            DataInputStream in = new DataInputStream(socket.getInputStream());
+            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+            String str = in.readUTF(); //получение сообщения от клиента
+            FileWriter wr = new FileWriter("cat1.ser");
+            wr.write(str);
+            //System.out.println("Сервер получил: " + str);
+            //out.writeUTF("Привет-привет..."); //ответ сервера
+            wr.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
 
